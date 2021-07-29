c07c1bd97f9bbfb862e4b55c79d2ddcabdf13b53
swyx <shawnthe1@gmail.com>
2021-07-29 08:05:37 -0700

    customized for albertsons

--- readme.md
+++ readme.md
@@ -4,12 +4,6 @@ this repo is meant to show how the go sdk offers the wrong error message when yo
 
 ## instructions
 
-1. have temporal server running
+1. have temporal server running (https://docs.temporal.io/docs/server/quick-install)
 2. ` go run worker/worker.go`
 3. `go run starter/starter.go`
-
-when running you should see this error: `PanicError reflect: Call with too few input arguments PanicStack activity for my-queue [panic]`
-
-This happens because `MyActivity` uses `workflow.Context` as its first argument, instead of Go's native `context.Context`. Once you fix this, the code runs as expected.
-
-The problem here is that the error `Call with too few input arguments` is incorrect/misleading.
--- workflow.go
+++ workflow.go
@@ -5,28 +5,38 @@ import (
 	"time"
 
 	"go.temporal.io/sdk/activity"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
-func MyWorkflow(ctx workflow.Context, name string) (string, error) {
+// depends on "go.temporal.io/sdk/activity"
+func FoodDeliveryWorkflow(ctx workflow.Context, name string) (string, error) {
 	logger := workflow.GetLogger(ctx)
-	logger.Info("Workflow MyWorkflow started")
+	logger.Info("Workflow FoodDelivery started")
 
 	ao := workflow.ActivityOptions {
 		StartToCloseTimeout: 	time.Minute,
+    RetryPolicy: 			&temporal.RetryPolicy{
+			InitialInterval:    time.Second,
+			BackoffCoefficient: 2,
+			MaximumInterval:    time.Minute * 10,
+			MaximumAttempts:    5,
+		},
 	}
 
 	ctx = workflow.WithActivityOptions(ctx, ao)
 	var result string
-	err := workflow.ExecuteActivity(ctx, MyActivity, name).Get(ctx, &result)
+	err := workflow.ExecuteActivity(ctx, ChargeStripeActivity, name).Get(ctx, &result)
 	if err != nil {
-		logger.Error("Activity failed", err)
+		logger.Error("ChargeStripeActivity failed", err)
 	}
 	return result, nil
 }
 
-func MyActivity(ctx context.Context, name string) (string, error) {
+// depends on "go.temporal.io/sdk/activity"
+// depends on "context"
+func ChargeStripeActivity(ctx context.Context, apikey string) (string, error) {
 	logger := activity.GetLogger(ctx)
-	logger.Info("MyActivity activity started")
-	return "hello " + name, nil
+	logger.Info("Activity apikey started")
+	return "apikey: " + apikey, nil
 }
